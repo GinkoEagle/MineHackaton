7edf1aa9802b0167c4a05bece7e552aece435dfb
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-04-14 14:54:03 -0700

    Update download script

--- download_images.sh
+++ download_images.sh
@@ -1,2 +1,2 @@
 # download data from Australia
-planet data search --item-type PSScene4Band --geom tulison.json --date acquired gt 2000-01-01 --date acquired lt 2018-03-31
+planet data download --item-type PSScene4Band --geom tulison.json --date acquired gt 2000-01-01 --date acquired lt 2018-03-31 --asset-type analytic
